0c0dfcc8bcd72418e999940b7ff14b8742ec300e
timer <330070781@qq.com>
2025-08-02 15:25:03 +0800

    vcpu and vpsci test for guest vm

--- guest/main.c
+++ guest/main.c
@@ -5,6 +5,9 @@
 
 __attribute__((aligned(SZ_4K))) char sp_stack[SZ_4K * NCPU] = {0};
 
+extern void _start();
+extern u64 smc_call(u64 funid, u64 target_cpu, u64 entrypoint);
+
 void print_logo(void)
 {
     printf(" V     V     M     M      II  \n");
@@ -15,12 +18,28 @@ void print_logo(void)
     printf("\n");
 }
 
+int vm_secondary_init()
+{
+    printf("I am guest secondary cpu\n");
+    while(1) {
+        printf("I am vm 1 on core 1\n");
+        for(int i=0; i < 100000000; i++);
+    }
+}
+
 int vm_primary_init()
 {
     pl011_init();
     print_logo();
 
-    while(1) {}
+    /* test code for wakeup vcore 1 */
+    smc_call((u64)0xc4000003, (u64)1, (u64)_start);
+
+    while(1) {
+        printf("I am vm 1 on core 0\n");
+        for(int i=0; i < 100000000; i++);
+    }
+
 
     return 0;
 }
